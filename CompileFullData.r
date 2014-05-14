42f21e61bb2bdc99b1a35ca4262d8c044fe06b1f
rajshah4 <rshah@pobox.com>
2014-05-14 18:53:15 -0500

    Fixed time conversion

--- CompileFullData.r
+++ CompileFullData.r
@@ -1,7 +1,7 @@
 setwd("~/Documents/R/chicrime/data/")
 print ("The CSV file should be named dffull.csv")
 df <- read.csv("~/Documents/R/chicrime/data/dffull.csv")
-df$PosixDate <- as.POSIXct(strptime(df$Date, format="%m/%d/%Y %H:%M"))
+df$PosixDate <- as.POSIXct(strptime(df$Date, format="%m/%d/%Y %I:%M:%S %p"))
 df <- df[c("Primary.Type", "Description", "Latitude", "Longitude", "PosixDate")]
 save(df,file="crimesfull.rda")
 setwd("~/Documents/R/chicrime/")
